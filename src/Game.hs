6b92d18e0358ac51a06177539053b55c8641b5c7
madjestic <madjestic13@gmail.com>
2020-11-12 11:58:26 +0100

    wip: adding multiple cameras support

--- src/Game.hs
+++ src/Game.hs
@@ -1,13 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE Arrows #-}
---  .d8888b.        d8888888b     d8888888888888 
--- d88P  Y88b      d888888888b   d8888888        
--- 888    888     d88P88888888b.d88888888        
--- 888           d88P 888888Y88888P8888888888    
--- 888  88888   d88P  888888 Y888P 888888        
--- 888    888  d88P   888888  Y8P  888888        
--- Y88b  d88P d8888888888888   "   888888        
---  "Y8888P88d88P     888888       8888888888888
 
 module Game
   ( Game    (..)
@@ -60,16 +52,7 @@ data Game =
        _debug    :: (Double, Double)
      , _options  :: Options
      , _gStg     :: Stage
-     , _objects  :: ObjectTree -- [Object] -- TODO: a tree of objects per game stage,
-                               -- a branch per render pass, i.e. GUI, backgground, foreground, etc.
--- | Maybe a better sollution is:
---   , _objects  :: [[Object]]
---                  [[GUI],[
--- it needs to be a tree-like structure:
--- GUI Objects: font, icons, buttons
--- Forground Objects : planets, spaceships, etc.
--- Background Objects: stars     
---     , _coobject :: Int -- shape of the [Object] data
+     , _objects  :: ObjectTree
      , _camera   :: Camera
      } deriving Show
 
