450d154a265133f73c3a66aee5b60010dbfa9546
madjestic <madjestic13@gmail.com>
2021-09-09 00:52:21 +0200

    working on binding a gen texture

---
+++ app/Grapher.hs
@@ -0,0 +1,218 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Main where
+
+import Control.Monad
+import Data.Massiv.Array as A hiding (windowSize)
+import Data.Massiv.Array.Unsafe  as AU
+import Data.Massiv.Array.Mutable as AM
+import Data.Word (Word8)
+import Graphics.UI.GLUT as GLUT (getArgsAndInitialize, mainLoop)
+import System.Exit (ExitCode(..), exitSuccess, exitWith)
+import Text.Read (readMaybe)
+
+import System.Random.SplitMix as SplitMix
+import Foreign.C (CInt(CInt))
+import Data.Text                              (Text, pack)
+import SDL
+import Graphics.GLUtil
+import Graphics.Rendering.OpenGL as GL
+import Unsafe.Coerce
+
+import Rendering
+import Application
+import Data.UUID    (nil)
+import Data.UUID.V1 (nextUUID)
+import qualified App
+import Object as O
+import Data.Maybe (fromMaybe)
+import Control.Lens.Fold (toListOf)
+
+graphRules :: Word8 -> Word8 -> Word8
+graphRules 0 3 = 1
+graphRules 1 2 = 1
+graphRules 1 3 = 1
+graphRules _ _ = 0
+
+graphStencil :: Stencil Ix2 Word8 Word8
+graphStencil = makeStencil (Sz (3 :. 3)) (1 :. 1) $ \ f ->
+  graphRules (f (0 :. 0))
+  (f (-1 :. -1) + f (-1 :. 0) + f (-1 :. 1) +
+   f ( 0 :. -1) +               f ( 0 :. 1) +
+   f ( 1 :. -1) + f ( 1 :. 0) + f ( 1 :. 1))
+
+graph :: Array S Ix2 Word8 -> Array S Ix2 Word8
+graph = compute . A.mapStencil Wrap graphStencil
+
+initLife :: Sz2 -> Array S Ix2 Word8 -> Array S Ix2 Word8
+initLife sz arr =
+  compute $
+  insertWindow
+    (makeArrayR D Par sz (const 0))
+    (Window ix0 (size arr) (index' arr . subtract ix0) Nothing)
+  where
+    ix0 = liftIndex (`div` 2) (unSz (sz - size arr))
+
+-- blinker :: Array S Ix2 Word8
+-- blinker = [ [0, 1, 0]
+--           , [0, 1, 0]
+--           , [0, 1, 0] ]
+
+
+-- glider :: Array S Ix2 Word8
+-- glider = [ [0, 1, 0]
+--          , [0, 0, 1]
+--          , [1, 1, 1] ]
+
+inf :: Array S Ix2 Word8
+inf =
+  [ [1, 1, 1, 0, 1]
+  , [1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1] ]
+
+inf2 :: Array S Ix2 Word8
+inf2 =
+  [ [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  , [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+  , [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
+  , [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]
+  , [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+  ]
+
+
+rand :: Array S Ix2 Word8
+rand = r''
+  where
+    r   = snd $ randomArrayS gen (Sz2 128 128) SplitMix.nextInt :: Array S Ix2 Int --(ParN 2) (Sz2 5 5)
+    r'  = A.map (toEnum . flip mod 2 . abs) r :: Array D Ix2 Word8
+    r'' = convert r'     :: Array S Ix2 Word8
+    gen = SplitMix.mkSMGen 0
+
+initResources :: Application -> Graph -> IO Application
+initResources app0 gph = do
+    uuid <- nextUUID
+    let
+      objs = introObjs ++ fntObjs ++ fgrObjs ++ bgrObjs
+      -- txs  = concat $ concatMap (toListOf (materials . traverse . M.textures)) objs -- :: [Texture]
+      hmap = [(fromMaybe nil uuid, 0::GLuint)]
+
+    putStrLn "Initializing Resources..."
+    putStrLn "Generating Textures..."
+    -- mapM_ (bindTextureObject hmap) txs
+    -- bindTextureObject (0::GLuint) 
+    putStrLn "Finished loading textures."
+
+    return app0 { _hmap = hmap }
+      where
+        introObjs = concat $ toListOf (App.objects . O.foreground)  (_intro app0) :: [Object]
+        fntObjs   = concat $ toListOf (App.objects . gui . O.fonts) (_main app0)  :: [Object]
+        fgrObjs   = concat $ toListOf (App.objects . O.foreground)  (_main app0)  :: [Object]
+        bgrObjs   = concat $ toListOf (App.objects . O.background)  (_main app0)  :: [Object]
+
+data Graph
+  =  Graph
+  { array  :: Array S Ix2 Word8
+  , marray :: MArray RealWorld S Ix2 Word8 }
+
+main :: IO ()
+main = do
+  let helpTxt =
+        "Usage:\n\
+                \   life [WIDTH HEIGHT] [SCALE]\n\
+                \ * WIDTH - number of cells horizontally (default 100)\n\
+                \ * HEIGHT - number of cells vertically (default 70)\n\
+                \ * SCALE - scaling factor, or how many pixels one cell should take on a screen\n\
+                \ example: \n\
+                \ cabal run exe:GameOfLife -- 35 35\n"
+  (_progName, args) <- getArgsAndInitialize
+  when (args == ["--help"]) $ putStrLn helpTxt >> exitSuccess
+  (m, n, s) <- case fmap readMaybe args of
+    [Just m, Just n, Just s]
+      | m > 0 && n > 0 && s > 0 -> return (m, n, s)
+    [Just m, Just n]
+      | m > 0 && n > 0 -> return (m, n, 10)
+    [] -> return (41, 50, 100)
+    --[] -> return (128, 128, 10)
+    _ -> do
+      putStrLn "Invalid arguments."
+      putStrLn helpTxt
+      exitWith $ ExitFailure 1
+
+  let title = "Game of Life" :: String
+      mc    = unsafeCoerce m :: CInt
+      nc    = unsafeCoerce n :: CInt
+      opts  = BackendOptions
+              { primitiveMode = Triangles
+              , bgrColor      = Color4 0.0 0.0 0.0 1.0
+              , ptSize        = 3.0
+              }
+      s      = 0 :: Int
+      sz     = Sz2 m n
+      iGraph = initGraph sz inf2 -- func
+      wSz    = size (pixelGrid s iGraph)
+
+  -- init a Mutable Array for storing graph data
+  mArr   <- newMArray' wSz :: IO (MArray RealWorld S Ix2 Word8)
+  window <- openWindow (pack title) (mc, nc)
+  drawGraph s mArr inf2
+    where
+      s = 1 :: Int
+  -- return ()
+  -- mainLoop opts window s sz mArr iGraph
+  -- where
+  --   mainLoop opts window s sz mArr arr = do
+  --     -- arr'  <- updateArray mArr arr
+  --     render opts window s sz mArr arr
+  --     mainLoop opts window s sz mArr arr
+
+--updateArray :: Int -> MArray RealWorld S Ix2 Word8 -> Array S Ix2 Word8 -> IO (Array S Ix2 Word8)
+--updateArray s mArr arr = do
+updateArray :: MArray RealWorld S Ix2 Word8 -> Array S Ix2 Word8 -> IO (Array S Ix2 Word8)
+updateArray mArr arr = do
+  return arr
+
+initGraph :: Sz2 -> Array S Ix2 Word8 -> Array S Ix2 Word8
+initGraph sz arr =
+  -- compute $ makeArrayR D Par sz (const 1)
+  -- compute $ makeArrayR D Par sz (const 1)
+  arr
